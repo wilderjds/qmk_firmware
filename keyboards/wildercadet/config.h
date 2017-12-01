91d0fb63f9628214f8c95b1bceb28ba5ca97fe59
Jacopo De Simoi <jacopods@gmail.com>
2017-12-01 17:45:28 -0500

    Add USB info

--- keyboards/wildercadet/config.h
+++ keyboards/wildercadet/config.h
@@ -21,12 +21,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
+#define VENDOR_ID       0x3700
+#define PRODUCT_ID      0x0037
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    You
-#define PRODUCT         wildercadet
-#define DESCRIPTION     A custom keyboard
+#define MANUFACTURER    wilder
+#define PRODUCT         (space-cadet)
+#define DESCRIPTION     A handwired space cadet keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 2
@@ -48,7 +48,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* COL2ROW, ROW2COL, or CUSTOM_MATRIX */
 #define DIODE_DIRECTION COL2ROW
- 
+
 // #define BACKLIGHT_PIN B7
 // #define BACKLIGHT_BREATHING
 // #define BACKLIGHT_LEVELS 3
