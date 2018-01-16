399bb36aa75f85c7ec98b877e910ba117e89e688
Jacopo De Simoi <jacopods@gmail.com>
2018-01-16 17:46:17 -0500

    Found the perfect product ID

--- keyboards/wildercadet/config.h
+++ keyboards/wildercadet/config.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x3700
-#define PRODUCT_ID      0x0037
+#define PRODUCT_ID      0x7E2 //2018
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    wilder
 #define PRODUCT         (space-cadet)
