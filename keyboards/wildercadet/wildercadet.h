ae08b4e9def3ef2ff38f8a0fbfc673ff66851c9d
Jacopo De Simoi <jacopods@gmail.com>
2018-02-20 15:30:04 -0500

    Use hex in wiring info

--- keyboards/wildercadet/wildercadet.h
+++ keyboards/wildercadet/wildercadet.h
@@ -23,18 +23,18 @@
 // The first section contains all of the arguments
 // The second converts the arguments into a two-dimensional array
 #define KEYMAP( \
-k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k010, k011, k012, k013, \
-k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k110, k111, k112, k113, \
-k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k210, k211, k213, \
-k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k310, k311, k312, k313, \
-k40, k42, k43, k44, k46, k48, k49, k410, k413 \
+k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D, \
+k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, \
+k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B     , k2D, \
+k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C, k3D, \
+k40     , k42, k43, k44     , k46     , k48, k49, k4A,           k4D \
 ) \
 { \
-{k00, k01,  k02, k03, k04, k05,  k06, k07,  k08, k09, k010,k011, k012, k013},	\
-{k10, k11,  k12, k13, k14, k15,  k16, k17,  k18, k19, k110,k111, k112, k113},	\
-{k20, k21,  k22, k23, k24, k25,  k26, k27,  k28, k29, k210,k211, KC_NO,k213},	\
-{k30, k31,  k32, k33, k34, k35,  k36, k37,  k38, k39, k310,k311, k312, k313},	\
-{k40, KC_NO,k42, k43, k44, KC_NO,k46, KC_NO,k48, k49, k410,KC_NO,KC_NO,k413},   \
+{k00, k01,  k02, k03, k04, k05,  k06, k07,  k08, k09, k0A,k0B, k0C, k0D},	\
+{k10, k11,  k12, k13, k14, k15,  k16, k17,  k18, k19, k1A,k1B, k1C, k1D},	\
+{k20, k21,  k22, k23, k24, k25,  k26, k27,  k28, k29, k2A,k2B, KC_NO,k2D},	\
+{k30, k31,  k32, k33, k34, k35,  k36, k37,  k38, k39, k3A,k3B, k3C, k3D},	\
+{k40, KC_NO,k42, k43, k44, KC_NO,k46, KC_NO,k48, k49, k4A,KC_NO,KC_NO,k4D},   \
 }
 
 #endif
