06b0bf7f6b60ad17d4474b244d74d3196ab31561
Jacopo De Simoi <jacopods@gmail.com>
2018-02-22 13:47:19 -0500

    Add correct wiring

--- keyboards/wildercadet/config.h
+++ keyboards/wildercadet/config.h
@@ -45,11 +45,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS { D0, D1, D2, D3, C6 }
+#define MATRIX_COL_PINS { C7, D5, D4, D6, D7, B4, B5, B6, F7, F6, F5, F4, F1, F0 }
 #define UNUSED_PINS
 
-/* COL2ROW, ROW2COL, or CUSTOM_MATRIX */
+/* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
 // #define BACKLIGHT_PIN B7
